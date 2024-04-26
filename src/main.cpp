4b5a666c6fece56d5805585c63343e5b6b8af416
Mukhammaddiyor Khomidov <diyor15152123@gmail.com>
2024-04-26 12:01:50 +0500

    First comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,7 @@
-
+#include <WiFi.h>
 #include <Arduino.h>
+#include <PubSubClient.h>
+#include <LiquidCrystal_I2C.h>
+#include <ArduinoJson.h>
 
-// Include necessary libraries here
-// TODO: Add necessary libraries here
 
-// Define global variables here
-// TODO: Add global variables here
-
-// Define functions here
-// TODO: Add functions here
-
-/**********************************/
-// SETUP //
-void setup(void) 
-{
-  // Initialize peripherals
-  // TODO: Initialize peripherals here
-  // TODO: LEDS, Buttons, LCD, etc.
-
-  // Connect to WiFi
-  // TODO: Connect to WiFi here
-
-  // Setup MQTT
-  // TODO: Setup MQTT here
-
-  // 
-
-}
-
-/**********************************/
-// LOOP //
-void loop(void) 
-{
-  // Keep WiFi and MQTT connection alive
-  // TODO: Keep WiFi and MQTT connection alive here
-}
