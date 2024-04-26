7ce8fef77b92bb585faabeb7e85465c1a1255761
Mukhammaddiyor Khomidov <diyor15152123@gmail.com>
2024-04-26 12:24:48 +0500

    Libraries and crredentials

--- src/main.cpp
+++ src/main.cpp
@@ -5,3 +5,21 @@
 #include <ArduinoJson.h>
 
 
+//Wifi credentials
+const char* ssid = "IoTExperiment";
+const char* password = "iotexperiment303ab";
+//mqtt credentials
+const char* mqtt_server = "mqtt.iotserver.uz"; // MQTT broker address
+const int mqtt_port = 1883; // MQTT broker port
+const char* mqtt_username = "userTTPU"; // Your unique MQTT username
+const char* mqtt_password = "mqttpass"; // Your unique MQTT username
+//mqtt client unique ID
+const char *mqtt_cleint_id = "ttpuIoTwiebeer"
+//mqtt topics
+const char *globalLedTopic = "ttpu/iot/global/led"
+const char *btnPressTopic = "ttpu/iot/wiebeer/led"
+//pinouts
+const int redPin = 32;
+const int yellowPin = 33;
+const int greenPin = 25;
+const int buttonPin = 25;
